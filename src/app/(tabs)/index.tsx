cc7edb71fb9f0cc5335660cb8da57a9858e6cc17
Jiawei Wu <robertwu1996@gmail.com>
2024-04-06 00:12:25 -0400

    implemet FlatList

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,40 +1,15 @@
-import products from '@/assets/data/products';
-import { View } from '@/src/components/Themed';
-import Colors from '@/src/constants/Colors';
-import { Image, StyleSheet, Text } from 'react-native';
+import products from "@/assets/data/products";
+import ProductListItem from "@/src/components/ProductListItem";
+import { FlatList } from "react-native";
 
-const product = products[0]
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image}/>
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 10
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: '600',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  price: {  
-    color: Colors.light.tint
-  },
-  image:{
-    width: "100%",
-    aspectRatio: 1
-  }
-});
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,48 @@
+import { View } from "@/src/components/Themed";
+import Colors from "@/src/constants/Colors";
+import { Image, StyleSheet, Text } from "react-native";
+import { Product } from "../types";
+
+const defaultImageUri =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: product.image || defaultImageUri }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    flex: 1,
+    maxWidth: "50%",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  price: {
+    color: Colors.light.tint,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
