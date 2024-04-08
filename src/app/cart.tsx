41d670292c7b07c41bcd30ec92e227273ce8fa54
Jiawei Wu <robertwu1996@gmail.com>
2024-04-08 16:39:07 -0400

    add total price functionality

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,17 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { FlatList, Platform, StyleSheet, View } from "react-native";
+import { FlatList, Platform, StyleSheet, Text, View } from "react-native";
+import Button from "../components/Button";
 import CartListItem from "../components/CartListItem";
 import { useCart } from "../providers/CartProvider";
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
   return (
     <View>
       <FlatList
         data={items}
         renderItem={({ item }) => <CartListItem cartItem={item} />}
       />
-
+      <Text style={{ marginTop: 20, fontSize: 30, fontStyle: "italic" }}>
+        Total: ${total}
+      </Text>
+      <Button text="Checkout" />
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
