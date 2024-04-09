8178b1d8f13e1377b2c770404fecaf932e6dcbda
Jiawei Wu <robertwu1996@gmail.com>
2024-04-09 18:23:53 -0400

    change admin order screen

---
+++ src/app/(admin)/orders/list/archive.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+const archive = () => {
+  return (
+    <View>
+      <Text>archive</Text>
+    </View>
+  );
+};
+export default archive;
+const styles = StyleSheet.create({});
--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,12 +1,10 @@
 import orders from "@/assets/data/orders";
 import OrderListItem from "@/src/components/OrderListItem";
-import { Stack } from "expo-router";
 import { FlatList } from "react-native";
 
 const ListOrders = () => {
   return (
     <>
-      <Stack.Screen options={{ title: "Orders" }} />
       <FlatList
         data={orders}
         renderItem={({ item }) => <OrderListItem order={item} key={item.id} />}
